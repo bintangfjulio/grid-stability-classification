64398ed6377a3338c05b1ccf1b7ebf193795fc66
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2023-01-26 13:17:49 +0700

    Update preprocessor.py

--- util/preprocessor.py
+++ util/preprocessor.py
@@ -15,9 +15,10 @@ class Preprocessor(pl.LightningDataModule):
         self.batch_size = batch_size
 
     def setup(self, stage=None):
-        train_set, test_set = self.preprocessor()   
+        train_set, valid_set, test_set = self.preprocessor()   
         if stage == "fit":
             self.train_set = train_set
+            self.valid_set = valid_set
         elif stage == "test":
             self.test_set = test_set
 
@@ -26,6 +27,14 @@ class Preprocessor(pl.LightningDataModule):
             dataset=self.train_set,
             batch_size=self.batch_size,
             shuffle=True,
+            num_workers=multiprocessing.cpu_count()
+        )
+    
+    def val_dataloader(self):
+        return DataLoader(
+            dataset=self.valid_set,
+            batch_size=self.batch_size,
+            shuffle=False,
             num_workers=multiprocessing.cpu_count()
         )
 
@@ -38,24 +47,50 @@ class Preprocessor(pl.LightningDataModule):
         )
 
     def preprocessor(self):
-        dataset = self.normalization(self.dataset)
+        if os.path.exists("dataset/train_set.pt") and os.path.exists("dataset/valid_set.pt") and os.path.exists("dataset/test_set.pt"):
+            print("\nLoading Data...")
+            train_set = torch.load("dataset/train_set.pt")
+            valid_set = torch.load("dataset/valid_set.pt")
+            test_set = torch.load("dataset/test_set.pt")
+            print('[ Loading Completed ]\n')
+        else:
+            print("\nPreprocessing Data...")
+            train_set, valid_set, test_set = self.preprocessing_data(self.dataset)
+            print('[ Preprocessing Completed ]\n')
+
+        return train_set, valid_set, test_set
+    
+    def preprocessing_data(self, dataset):
+        dataset = self.normalization(dataset)
         X_train_res, y_train_res = self.oversampling(dataset)
         y_train_res = self.label_encoding(y_train_res)
 
-        X_train, X_test, y_train, y_test = train_test_split(X_train_res, y_train_res, test_size=0.3, random_state=42)
-        
+        X_train_valid, X_test, y_train_valid, y_test = train_test_split(X_train_res, y_train_res, test_size=0.2, random_state=42)
+        X_train, X_valid, y_train, y_valid = train_test_split(X_train_valid, y_train_valid, test_size=0.1, random_state=42)
+    
         X_train_tensor = torch.from_numpy(X_train.values).float()
         y_train_tensor = torch.from_numpy(y_train.values.ravel()).float()
+        
+        X_valid_tensor = torch.from_numpy(X_valid.values).float()
+        y_valid_tensor = torch.from_numpy(y_valid.values.ravel()).float()
+        
         X_test_tensor = torch.from_numpy(X_test.values).float()
         y_test_tensor = torch.from_numpy(y_test.values.ravel()).float()
 
         y_train_tensor = y_train_tensor.unsqueeze(1)
         train_set = TensorDataset(X_train_tensor, y_train_tensor)
+        
+        y_valid_tensor = y_valid_tensor.unsqueeze(1)
+        valid_set = TensorDataset(X_valid_tensor, y_valid_tensor)
 
         y_test_tensor = y_test_tensor.unsqueeze(1)
         test_set = TensorDataset(X_test_tensor, y_test_tensor)
+       
+        torch.save(train_set, "dataset/train_set.pt")
+        torch.save(valid_set, "dataset/valid_set.pt")
+        torch.save(test_set, "dataset/test_set.pt")
 
-        return train_set, test_set
+        return train_set, valid_set, test_set
 
     def normalization(self, dataset):
         dataset['tau1 Z'] = zscore(dataset['tau1'])
