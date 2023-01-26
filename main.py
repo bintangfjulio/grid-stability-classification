af55094dccf74c48b224069fd649b5a055493c27
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2023-01-26 12:51:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import pytorch_lightning as pl
 
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 from util.preprocessor import Preprocessor
+from model.bilstm import BiLSTM
 
 if __name__ == "__main__":
     pl.seed_everything(42)
 
     module = Preprocessor(batch_size=100)
-    module.preprocessor()
+    model = BiLSTM(lr=1e-5, input_size=0, hidden_size=0)
+    
+    checkpoint_callback = ModelCheckpoint(dirpath='./checkpoints/bilstm_result', monitor='val_loss')
+    logger = TensorBoardLogger('log', name='bilstm_result')
+    early_stop_callback = EarlyStopping(monitor='val_loss', min_delta=0.00, check_on_train_epoch_end=1, patience=10)
+
+    trainer = pl.Trainer(
+        accelerator='gpu',
+        max_epochs=200,
+        default_root_dir='./checkpoints/bilstm_result',
+        callbacks = [checkpoint_callback, early_stop_callback],
+        deterministic=True,
+        logger=logger)
+    
+    trainer.fit(model=model, datamodule=module)
+    trainer.test(model=model, datamodule=module, ckpt_path='best')
