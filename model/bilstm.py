e6048bb72a6ba0b649514428520c52a672cd30b9
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2023-01-26 11:01:30 +0700

    Create bilstm.py

--- model/bilstm.py
+++ model/bilstm.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import pytorch_lightning as pl
 
+from torchmetrics.classification import MulticlassAccuracy
+
 class BiLSTM(pl.LightningModule):
     def __init__(self, num_classes, lr, input_size, hidden_size, dropout=0.5, num_layers=2):
         super(BiLSTM, self).__init__()
@@ -31,46 +33,43 @@ class BiLSTM(pl.LightningModule):
         return optimizer
       
     def training_step(self, train_batch, batch_idx):
-        input_ids, target = train_batch
+        X, target = train_batch
 
-        preds = self.model(input_ids=input_ids)
+        preds = self(X)
         loss = self.criterion(preds, target=target.float())
 
         max_pred_idx = preds.argmax(1)
         max_target_idx = target.argmax(1)
         accuracy = self.accuracy_metric(max_pred_idx, max_target_idx)
-        mcc = self.mcc_metric(max_pred_idx, max_target_idx)
 
-        self.log_dict({'train_loss': loss, 'train_accuracy': accuracy, 'train_mcc': mcc}, prog_bar=True, on_epoch=True)
+        self.log_dict({'train_loss': loss, 'train_accuracy': accuracy}, prog_bar=True, on_epoch=True)
 
         return loss
 
     def validation_step(self, valid_batch, batch_idx):
-        input_ids, target = valid_batch
+        X, target = valid_batch
 
-        preds = self.model(input_ids=input_ids)
+        preds = self(X)
         loss = self.criterion(preds, target=target.float())
 
         max_pred_idx = preds.argmax(1)
         max_target_idx = target.argmax(1)
         accuracy = self.accuracy_metric(max_pred_idx, max_target_idx)
-        mcc = self.mcc_metric(max_pred_idx, max_target_idx)
 
-        self.log_dict({'val_loss': loss, 'val_accuracy': accuracy, 'val_mcc': mcc}, prog_bar=True, on_epoch=True)
+        self.log_dict({'val_loss': loss, 'val_accuracy': accuracy}, prog_bar=True, on_epoch=True)
 
         return loss
 
     def test_step(self, test_batch, batch_idx):
-        input_ids, target = test_batch
+        X, target = test_batch
 
-        preds = self.model(input_ids=input_ids)
+        preds = self(X)
         loss = self.criterion(preds, target=target.float())
 
         max_pred_idx = preds.argmax(1)
         max_target_idx = target.argmax(1)
         accuracy = self.accuracy_metric(max_pred_idx, max_target_idx)
-        mcc = self.mcc_metric(max_pred_idx, max_target_idx)
 
-        self.log_dict({'test_loss': loss, 'test_accuracy': accuracy, 'test_mcc': mcc}, prog_bar=True, on_epoch=True)
+        self.log_dict({'test_loss': loss, 'test_accuracy': accuracy}, prog_bar=True, on_epoch=True)
 
         return loss
