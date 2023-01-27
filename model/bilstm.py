aff6a319b2077253f7cb828c3e33a23f17474900
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2023-01-27 13:02:03 +0700

    Update bilstm.py

--- model/bilstm.py
+++ model/bilstm.py
@@ -9,7 +9,7 @@ class BiLSTM(pl.LightningModule):
         super(BiLSTM, self).__init__()
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, bidirectional=False, batch_first=True)
         self.dropout = nn.Dropout(dropout)
-        self.output_layer = nn.Linear(hidden_size * 2, num_classes)
+        self.output_layer = nn.Linear(hidden_size, num_classes)
         self.sigmoid = nn.Sigmoid()
         self.criterion = nn.BCELoss()
         self.accuracy_metric = BinaryAccuracy()
