8a7137b2925dd6fe43d14dd9b98d7c66417bc42e
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2023-01-28 09:38:21 +0700

    fix getter feature size

--- util/preprocessor.py
+++ util/preprocessor.py
@@ -119,7 +119,7 @@ class Preprocessor(pl.LightningDataModule):
         return X_train, y_train
 
     def get_feature_size(self):
-        X = self.dataset['tau1','tau2','tau3','tau4','p1', 'p2', 'p3', 'p4','g1','g2','g3','g4']
+        X = self.dataset[['tau1','tau2','tau3','tau4','p1', 'p2', 'p3', 'p4','g1','g2','g3','g4']]
         y = self.dataset['stabf']
         
         return len(y.drop_duplicates().values.tolist()), len(X.columns.tolist())
