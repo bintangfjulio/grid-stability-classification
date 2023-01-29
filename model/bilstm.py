ec31dfd9621a09cd8cf887006b741f91784bf78b
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2023-01-29 16:11:42 +0700

    Update bilstm.py

--- model/bilstm.py
+++ model/bilstm.py
@@ -2,67 +2,15 @@ import torch
 import torch.nn as nn
 import pytorch_lightning as pl
 
-from torchmetrics.classification import BinaryAccuracy
-
 class BiLSTM(pl.LightningModule):
-    def __init__(self, lr, num_classes, input_size, hidden_size=300, num_layers=2, dropout=0.5):
+    def __init__(self, num_classes, input_size, hidden_size=300, num_layers=2, dropout=0.5):
         super(BiLSTM, self).__init__()
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, bidirectional=True, batch_first=True)
         self.dropout = nn.Dropout(dropout)
         self.output_layer = nn.Linear(hidden_size * 2, num_classes)
-        self.criterion = nn.BCEWithLogitsLoss()
-        self.accuracy_metric = BinaryAccuracy()
-        self.lr = lr
 
     def forward(self, X):
         lstm_output, _ = self.lstm(X)
         preds = self.output_layer(self.dropout(lstm_output))
 
         return preds
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
-
-        return optimizer
-
-    def training_step(self, train_batch, batch_idx):
-        X, target = train_batch
-
-        preds = self(X)
-        loss = self.criterion(preds, target=target.float())
-
-        max_pred_idx = preds.argmax(1)
-        max_target_idx = target.argmax(1)
-        accuracy = self.accuracy_metric(max_pred_idx, max_target_idx)
-
-        self.log_dict({'train_loss': loss, 'train_accuracy': accuracy}, prog_bar=True, on_epoch=True)
-
-        return loss
-
-    def validation_step(self, valid_batch, batch_idx):
-        X, target = valid_batch
-
-        preds = self(X)
-        loss = self.criterion(preds, target=target.float())
-
-        max_pred_idx = preds.argmax(1)
-        max_target_idx = target.argmax(1)
-        accuracy = self.accuracy_metric(max_pred_idx, max_target_idx)
-
-        self.log_dict({'val_loss': loss, 'val_accuracy': accuracy}, prog_bar=True, on_epoch=True)
-
-        return loss
-
-    def test_step(self, test_batch, batch_idx):
-        X, target = test_batch
-
-        preds = self(X)
-        loss = self.criterion(preds, target=target.float())
-
-        max_pred_idx = preds.argmax(1)
-        max_target_idx = target.argmax(1)
-        accuracy = self.accuracy_metric(max_pred_idx, max_target_idx)
-
-        self.log_dict({'test_loss': loss, 'test_accuracy': accuracy}, prog_bar=True, on_epoch=True)
-
-        return loss
