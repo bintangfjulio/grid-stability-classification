fd1b7036fd54a4378e55de106afac356fc804b7b
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2023-01-29 16:17:05 +0700

    Update classifier.py

--- util/classifier.py
+++ util/classifier.py
@@ -2,14 +2,16 @@ import torch
 import torch.nn as nn
 import pytorch_lightning as pl
 
+from model.bilstm import BiLSTM
 from torchmetrics.classification import BinaryAccuracy
 
 class Classifier(pl.LightningModule):
-    def __init__(self, lr, num_classes):
+    def __init__(self, lr, num_classes, input_size):
         super(Classifier, self).__init__()
         self.criterion = nn.BCEWithLogitsLoss()
         self.accuracy_metric = BinaryAccuracy()
         self.lr = lr
+        self.model = BiLSTM(num_classes=num_classes, input_size=input_size)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
@@ -19,7 +21,7 @@ class Classifier(pl.LightningModule):
     def training_step(self, train_batch, batch_idx):
         X, target = train_batch
 
-        preds = self(X)
+        preds = self.model(X)
         loss = self.criterion(preds, target=target.float())
 
         max_pred_idx = preds.argmax(1)
@@ -33,7 +35,7 @@ class Classifier(pl.LightningModule):
     def validation_step(self, valid_batch, batch_idx):
         X, target = valid_batch
 
-        preds = self(X)
+        preds = self.model(X)
         loss = self.criterion(preds, target=target.float())
 
         max_pred_idx = preds.argmax(1)
@@ -47,7 +49,7 @@ class Classifier(pl.LightningModule):
     def test_step(self, test_batch, batch_idx):
         X, target = test_batch
 
-        preds = self(X)
+        preds = self.model(X)
         loss = self.criterion(preds, target=target.float())
 
         max_pred_idx = preds.argmax(1)
