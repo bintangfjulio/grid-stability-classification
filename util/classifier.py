02165d9f95f66cf629b37d37625eadbcc970205a
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2023-01-29 16:13:38 +0700

    Update classifier.py

--- util/classifier.py
+++ util/classifier.py
@@ -1,1 +1,59 @@
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
 
+from torchmetrics.classification import BinaryAccuracy
+
+class Classifier(pl.LightningModule):
+    def __init__(self, lr, num_classes):
+        super(Classifier, self).__init__()
+        self.criterion = nn.BCEWithLogitsLoss()
+        self.accuracy_metric = BinaryAccuracy()
+        self.lr = lr
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+
+        return optimizer
+
+    def training_step(self, train_batch, batch_idx):
+        X, target = train_batch
+
+        preds = self(X)
+        loss = self.criterion(preds, target=target.float())
+
+        max_pred_idx = preds.argmax(1)
+        max_target_idx = target.argmax(1)
+        accuracy = self.accuracy_metric(max_pred_idx, max_target_idx)
+
+        self.log_dict({'train_loss': loss, 'train_accuracy': accuracy}, prog_bar=True, on_epoch=True)
+
+        return loss
+
+    def validation_step(self, valid_batch, batch_idx):
+        X, target = valid_batch
+
+        preds = self(X)
+        loss = self.criterion(preds, target=target.float())
+
+        max_pred_idx = preds.argmax(1)
+        max_target_idx = target.argmax(1)
+        accuracy = self.accuracy_metric(max_pred_idx, max_target_idx)
+
+        self.log_dict({'val_loss': loss, 'val_accuracy': accuracy}, prog_bar=True, on_epoch=True)
+
+        return loss
+
+    def test_step(self, test_batch, batch_idx):
+        X, target = test_batch
+
+        preds = self(X)
+        loss = self.criterion(preds, target=target.float())
+
+        max_pred_idx = preds.argmax(1)
+        max_target_idx = target.argmax(1)
+        accuracy = self.accuracy_metric(max_pred_idx, max_target_idx)
+
+        self.log_dict({'test_loss': loss, 'test_accuracy': accuracy}, prog_bar=True, on_epoch=True)
+
+        return loss
