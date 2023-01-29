217fcaac22e48de882325dbadc757bcb3b2decbe
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2023-01-29 16:31:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
+import argparse
 import pytorch_lightning as pl
 
+from utils.preprocessor import Preprocessor
+from utils.classifier import Classifier
+from utils.regressor import Regressor
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
-from util.preprocessor import Preprocessor
-from util.classifier import Classifier
 
 if __name__ == "__main__":
     pl.seed_everything(42)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mining', choices=['klasifikasi', 'regresi'], required=True, help='Pilihan data mining')
+    
+    args = parser.parse_args()
+    config = vars(args)
 
     module = Preprocessor(batch_size=64)
     num_classes, input_size = module.get_feature_size()
     
-    model = Classifier(lr=1e-3, num_classes=num_classes, input_size=input_size)
+    if config['mining'] == 'klasifikasi':
+        model = Classifier(lr=1e-3, num_classes=num_classes, input_size=input_size)
+    elif config['mining'] == 'regresi':
+        model = Regressor()
     
-    checkpoint_callback = ModelCheckpoint(dirpath='./checkpoints/bilstm_result', monitor='val_loss')
-    logger = TensorBoardLogger('log', name='bilstm_result')
+    checkpoint_callback = ModelCheckpoint(dirpath=f'./checkpoints/{config['mining']}_bilstm_result', monitor='val_loss')
+    logger = TensorBoardLogger('log', name=f'{config['mining']}_bilstm_result')
     early_stop_callback = EarlyStopping(monitor='val_loss', min_delta=0.00, check_on_train_epoch_end=1, patience=10)
 
     trainer = pl.Trainer(
         accelerator='gpu',
         max_epochs=100,
-        default_root_dir='./checkpoints/bilstm_result',
+        default_root_dir=f'./checkpoints/{config['mining']}_bilstm_result',
         callbacks = [checkpoint_callback, early_stop_callback],
         deterministic=True,
         logger=logger)
