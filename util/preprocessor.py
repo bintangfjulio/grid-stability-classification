1fe9fa6778c07caa35077aa387e1c580c8b34639
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2023-01-29 11:35:30 +0700

    Update preprocessor.py

--- util/preprocessor.py
+++ util/preprocessor.py
@@ -63,13 +63,12 @@ class Preprocessor(pl.LightningDataModule):
         return train_set, valid_set, test_set
     
     def preprocessing_data(self, dataset):
-        dataset = self.normalization(dataset)
-        
         # X_train_res, y_train_res = self.oversampling(dataset)
 
         X_train_res = dataset[['tau1','tau2','tau3','tau4','p1', 'p2', 'p3', 'p4','g1','g2','g3','g4']]
         y_train_res = self.dataset['stabf']
 
+        X_train_res = self.normalization(X_train_res)
         y_train_res = self.label_encoding(y_train_res)
 
         X_train_valid, X_test, y_train_valid, y_test = train_test_split(X_train_res, y_train_res, test_size=0.2, random_state=42)
