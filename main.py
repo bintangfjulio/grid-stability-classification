660b47b53cc9459c09f9862af9a5c65d03616360
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2023-01-29 16:38:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -18,19 +18,21 @@ if __name__ == "__main__":
     module = Preprocessor(batch_size=64)
     num_classes, input_size = module.get_feature_size()
     
-    if config['mining'] == 'klasifikasi':
+    mining = config['mining']
+    
+    if mining == 'klasifikasi':
         model = Classifier(lr=1e-3, num_classes=num_classes, input_size=input_size)
-    elif config['mining'] == 'regresi':
+    elif mining == 'regresi':
         model = Regressor()
     
-    checkpoint_callback = ModelCheckpoint(dirpath=f'./checkpoints/{config['mining']}_bilstm_result', monitor='val_loss')
-    logger = TensorBoardLogger('log', name=f'{config['mining']}_bilstm_result')
+    checkpoint_callback = ModelCheckpoint(dirpath=f'./checkpoints/{mining}_bilstm_result', monitor='val_loss')
+    logger = TensorBoardLogger('log', name=f'{mining}_bilstm_result')
     early_stop_callback = EarlyStopping(monitor='val_loss', min_delta=0.00, check_on_train_epoch_end=1, patience=10)
 
     trainer = pl.Trainer(
         accelerator='gpu',
         max_epochs=100,
-        default_root_dir=f'./checkpoints/{config['mining']}_bilstm_result',
+        default_root_dir=f'./checkpoints/{mining}_bilstm_result',
         callbacks = [checkpoint_callback, early_stop_callback],
         deterministic=True,
         logger=logger)
